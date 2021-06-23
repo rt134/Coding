7ebbc9503f8076accda5a20a8fb9cc9abc046181
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-06-23 13:01:42 +0530

    23jun

--- DP/Kp.cpp
+++ DP/Kp.cpp
@@ -19,7 +19,27 @@ int Kp(int wt[], int v[], int w, int n){
 
 // Top Down
 int tKp(int wt[],int v[],int w, int n){
-    return 0;
+    int t[n+1][w+1];
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<w+1;j++){
+            if(j == 0 || i == 0){
+                t[i][j] = 0;
+            }
+        }
+    }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<w+1;j++){
+            if(wt[i-1] <= j ){
+                t[i][j] = max((v[i-1]+t[i-1][j-wt[i-1]]),t[i-1][j]);
+            }else{
+                t[i][j] = t[i-1][j];
+            }
+        }
+
+    }
+    return t[n][w];
 }
 
 
@@ -38,7 +58,8 @@ int main() {
         cin>>v[i];
     }
     
-    cout<<Kp(wt,v,w,n);
+    cout<<"Recursive "<<Kp(wt,v,w,n)<<endl;
+    cout<<"Top down "<<tKp(wt,v,w,n)<<endl;
 
 	
 	return 0;
