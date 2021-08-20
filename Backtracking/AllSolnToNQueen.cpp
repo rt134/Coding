df668b14da730ee62412c88122ddef6909734c0a
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-08-20 17:21:12 +0530

    20aug

--- Backtracking/AllSolnToNQueen.cpp
+++ Backtracking/AllSolnToNQueen.cpp
@@ -2,59 +2,64 @@
 #include<vector>
 using namespace std;
 
-bool check(vector<vector<int>> board, int col, int row){
-    int n = board.size();
-
-    // Col will be distinct
-
-    // Checking for row
-    for(int i=0;i<col;i++){
-        if(board[row][i] == 1){
+bool isSafe(vector<vector<int> > board,int row, int col)
+{
+    int i, j;
+    int N = board.size();
+ 
+    for (i = 0; i < col; i++){
+        if (board[row][i] == 1){
             return false;
         }
     }
-
-    int i,j;
-    // Checking upper diagonal
-    for(i = row,j = col;i>=0,j>=0;i--,j--){
-        if(board[i][j] == 1){
+ 
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--){
+        if (board[i][j] == 1){
             return false;
         }
     }
-
-    // Checking Lower diagonal
-    for(i = row,j = col;i<n,j>=0;i++,j--){
-        if(board[i][j] == 1){
+ 
+    for (i = row, j = col; j >= 0 && i < N; i++, j--){
+        if (board[i][j] == 1){
             return false;
         }
     }
-
     return true;
 }
 
-bool NQSoln(vector<vector<int>>& board,int col, int size){
+void PrintBoard(vector<vector<int>> v){
+
+    for(int i=0;i<v.size();i++){
+        for(int j=0;j<v.size();j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
+
+void NQSoln(vector<vector<int>> board,int col, int size){
 
     if(col == size){
         for(int i=0;i<size;i++){
-            if(check(board,col,i)){
-                return true;
-            }
-        }
-    }
-
-    bool res = false;
-    for(int i=0;i<size;i++){
-        if(check(board,col,i)){
             board[i][col] = 1;
-            res = NQSoln(board,col+1,size) || res;
+            if(isSafe(board,i,col)){
+                PrintBoard(board);
+            }
             board[i][col] = 0;
         }
+        return;
     }
 
-
-
-    return res;
+    for(int i=0;i<size;i++){
+        board[i][col] = 1;
+        NQSoln(board,col+1,size);
+        board[i][col] = 0;
+    }
+    
 }
+
+
 
 
 int main(){
@@ -66,26 +71,16 @@ int main(){
 
     for(int i=0;i<n;i++){
         vector<int> v;
-        for(int j=0;i<n;j++){
+        for(int j=0;j<n;j++){
             v.push_back(0);
         }
         board.push_back(v);        
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;i<n;j++){
-            cout<<board[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    cout<<"Input Board"<<endl;
+    PrintBoard(board);
     
-    for(int i=0;i<n;i++){
-        cout<<"i ";
-        if(NQSoln(board,i,n)){
-            count++;
-        }
-    }
-    cout<<count<<endl;
+    NQSoln(board,0,n);
 
     return 0;
 }
--- Template.cpp
+++ Template.cpp
@@ -10,6 +10,16 @@ int main() {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    time_t start, end;
+    
+
+    time(&start);
+    // Function
+    time(&end);
+    
+    double time_taken = double(end - start);
+    cout << "Time taken is : " << fixed << time_taken << setprecision(5);
+    cout << " sec " << endl;
 	
 	return 0;
 }
