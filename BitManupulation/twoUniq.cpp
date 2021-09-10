5576ae7d0d02fae261eb6bfa2db1b95671abae75
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-09-10 16:56:17 +0530

    10sep

--- BitManupulation/twoUniq.cpp
+++ BitManupulation/twoUniq.cpp
@@ -10,16 +10,41 @@ int main() {
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    time_t start, end;
-    
+    int n;
+    cin>>n;
+    int arr[n];
 
-    time(&start);
-    // Function
-    time(&end);
-    
-    double time_taken = double(end - start);
-    cout << "Time taken is : " << fixed << time_taken << setprecision(5);
-    cout << " sec " << endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int sum = 0;
+
+    for(int i=0;i<n;i++){
+        sum^= arr[i];
+    }
+
+    int idx;
+    for(int i=0;i<32;i++){
+        if(((1<<i)&sum) != 0){
+            idx = i;
+            break;
+        }
+    }
+
+    int first=0,second=0;
+
+    int mask = 1<<idx;
+
+    for(int i=0;i<n;i++){
+        if((arr[i]&mask) == 0){
+            first ^= arr[i];
+        }else{
+            second ^= arr[i];
+        }
+    }
+
+    cout<<"First "<<first<<" second "<<second<<endl;
 	
 	return 0;
 }
