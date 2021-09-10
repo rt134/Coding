92b25c6df1259d35fb138a0ac28cf24b278e0f54
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-09-10 16:40:15 +0530

    10sepaug

---
+++ BitManupulation/twoUniq.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    time_t start, end;
+    
+
+    time(&start);
+    // Function
+    time(&end);
+    
+    double time_taken = double(end - start);
+    cout << "Time taken is : " << fixed << time_taken << setprecision(5);
+    cout << " sec " << endl;
+	
+	return 0;
+}
